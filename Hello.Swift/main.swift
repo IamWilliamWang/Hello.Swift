fcde8a8f232c8fb19d98035ed72a7083226dd1c8
William Wang <william@williamdemac.local>
2016-03-12 13:26:08 +0800

    Initial Commit

---
+++ Hello.Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello.Swift
+//
+//  Created by William Wang on 16/3/12.
+//  Copyright © 2016年 William Wang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
