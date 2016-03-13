c627f8af4456db811a3e81a11659c0c2eb553f8b
IamWilliamWang <iamjerichoholic@hotmail.com>
2016-03-13 18:36:52 +0800

    Hello Swift
    my first program with Swift.

--- Hello.Swift/main.swift
+++ Hello.Swift/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+func say(name : String) {
+    print("\(name)")
+}
+let pi = 3.1415926
+var r:Double = 0.0
+var str = "Hello, "
+str += "Swift!"
+say(str)
 
+var arr = ["Hello,","Swift",1,2,3]
+print(arr)
+
+var arr1 = []
+var arr2 = [String]()
+
+var dict = ["number":101,"name":"person"]
+dict["age"] = 20
+print(dict)
+print(dict["age"])
+for(key,value) in dict {
+    print("\(value)")
+}
+
+for var index in 0...4 {
+    arr2.append("\(index)")
+}
+
+for var i in arr2 {
+    print(i)
+}
+
+for var index=0; index < 3; index++ {
+    print("Item \(index)")
+}
+
+func getNums()->(Int,String,Bool) {
+    return(1,"IamString",true)
+}
